bc0f76b3c76deb6d6e6f3eca811c95fb9f4f8277
blcrouch <blcrouch@gmail.com>
2015-04-20 15:06:32 -0400

    Create Hello-world.java

---
+++ Hello-world.java
@@ -0,0 +1,15 @@
+/**
+ * Hello, world app with some additional lines
+ * 
+ * Benjamin Crouch
+ * 1.0
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+        System.out.println("Hello, everybody!");
+        System.out.println("全世界, 大家好!");
+    }
+}
